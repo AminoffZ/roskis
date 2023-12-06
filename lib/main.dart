7e362ba9015403d9f2250360933995f83bb6180f
AminoffZ <mouiylus@gmail.com>
2023-12-06 16:10:51 +0200

    🌍 Add map, placeholder auth buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:roskis/map.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,12 +24,42 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  signIn() {
+    if (kDebugMode) {
+      print("Sign in");
+    }
+  }
+
+  createAccount() {
+    if (kDebugMode) {
+      print("Create account");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Roskis')),
-      body: const Center(child: Text('Welcome to Roskis!')),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(onPressed: signIn, child: const Text("Sign In")),
+          TextButton(
+              onPressed: createAccount, child: const Text("Create Account")),
+        ],
+      )),
+    );
+  }
+}
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: RoskisMap(),
     );
   }
 }
